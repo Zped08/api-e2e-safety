a8783e59e3248304b5bda935d6c2b2b1d0c70ee0
Jair Flores <jairfloresa0802@gmail.com>
2023-06-21 12:24:29 +0200

    configurando pothos

--- src/builder.ts
+++ src/builder.ts
@@ -1,4 +1,18 @@
-
-
 import SchemaBuilder from "@pothos/core";
-export const builder = new SchemaBuilder({});
+import { DateResolver } from "graphql-scalars";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import { prisma } from "./db";
+export const builder = new SchemaBuilder<{
+  Scalars: {
+    Date: { Input: Date; Output: Date };
+  };
+  PrismaTypes: PrismaTypes;
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
+
+builder.addScalarType("Date", DateResolver, {});
