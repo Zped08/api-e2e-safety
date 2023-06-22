c87dba4f57a44a9ebfa3820dc182d9f47726da89
Jair Flores <jairfloresa0802@gmail.com>
2023-06-22 14:08:53 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-//import { createServer } from "node:http";
-const http = require('node:http');
+import { createServer } from "node:http";
+//const http = require('node:http');
 
 import { createYoga } from "graphql-yoga";
 import { schema } from "./schema";
 //algo
 const yoga = createYoga({ schema });
 
-const server = http.createServer(yoga);
+const server = createServer(yoga);
 
 server.listen(4000, () => {
   console.log(`🚀 GraphQL Server ready at http://localhost:4000/graphql`);
